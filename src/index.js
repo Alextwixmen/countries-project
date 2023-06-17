deb2c95f8ac20acbeb6bbfb5aed5490a3c95e7a1
Alexander <alexanderlysenko99@mail.ru>
2023-06-17 15:20:03 +0300

    app-theme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { store } from './store';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById('root')
 );
